d6cf325dfdd91166010fcc6aae7cfee1d7c2e4b5
andela-tisrael <itomilayo@gmail.com>
2016-11-21 10:57:24 +0100

    write test for the inverted index class

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict'
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+/**
+ * setting up browserSync to our server directory,
+ *  which our root directory in this case src
+ */
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: "./jasmine/SpecRunner.html"
+        }
+    });
+});
+
+/**
+ * adding watch function to be able to reload our browser
+ *  whenever changes are been made to js, html, and css files
+ */
+gulp.task('watch', ['browserSync'], function() {
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('src/*.css', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('*.js', browserSync.reload);
+    gulp.watch('spec/*.js', browserSync.reload);
+    gulp.watch('frontend/src/*.js', browserSync.reload);
+});
