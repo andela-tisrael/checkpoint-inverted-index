51706f64634f518de426a2f6dc1d4e135500cf43
andela-tisrael <itomilayo@gmail.com>
2016-11-22 23:05:11 +0100

    edit eslintrc for code climate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 {
+  "extends": "airbnb",
+  "root": true,
   "env": {
-    "node": true
+    "node": true,
+    "es6": true,
+    "mocha": true
   },
   "rules": {
-    "brace-style": [2, "1tbs", { "allowSingleLine": true }],
-    "comma-dangle": [2, "never"],
-    "comma-style": [2, "first", { exceptions: {ArrayExpression: true, ObjectExpression: true} }],
-    "complexity": [2, 6],
-    "curly": 2,
-    "eqeqeq": [2, "allow-null"],
-    "max-statements": [2, 30],
-    "no-shadow-restricted-names": 2,
-    "no-undef": 2,
-    "no-use-before-define": 2,
-    "radix": 2,
-    "semi": 2,
-    "space-infix-ops": 2,
-    "strict": 0
-  },
-  "globals": {
-    "AnalysisView": true,
-    "PollingView": true,
-    "Prism": true,
-    "Spinner": true,
-    "Timer": true,
-    "moment": true
+    "one-var": 0,
+    "one-var-declaration-per-line": 0,
+    "new-cap": 0,
+    "consistent-return": 0,
+    "no-param-reassign": 0,
+    "comma-dangle": 0,
+    "no-shadow": [0, { "allow": ["req", "res", "err"] }],
+    "valid-jsdoc": [0, {
+      "requireReturn": true,
+      "requireReturnType": true,
+      "requireParamDescription": false,
+      "requireReturnDescription": true
+    }],
+    "require-jsdoc": [0, {
+        "require": {
+            "FunctionDeclaration": true,
+            "MethodDefinition": true,
+            "ClassDeclaration": true
+        }
+    }]
   }
 }
