40fc410afd264c3b9d09155fda3c62e720a9fca8
andela-tisrael <itomilayo@gmail.com>
2016-11-22 22:25:38 +0100

    implementing each functions

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -17,6 +17,30 @@ class InvertedIndex {
    * @return
    */
   createIndex(jsonArray) {
+    this.indexMap = {};
+    this.docCount = [];
+    this.count = 0;
+    for (const object in jsonArray) {
+        this.docCount.push(parseInt(object));
+        }
+    jsonArray.forEach((object) => {
+      this.count += 1;
+      let bookTitleArray = InvertedIndex.token((jsonArray[object].title));
+      let bookTextArray = InvertedIndex.token((jsonArray[object].text));
+      let wordIndex = new Set(bookTitleArray.concat(bookTextArray));
+      let formArray = Array.from(wordIndex.values());
+      this.mapWords(formArray, this.count);
+    });
+  }
+  /**
+   * Get tokens in a string of text.
+   * @function
+   * @param {String} jsonWord [word to be tokenize.]
+   * @return {Array} [array of tokens]
+   */
+  static token(jsonWord) {
+    return jsonWord.toLowerCase()
+    .match(/\w+/g);
   }
   /**
    * Map Array of words to indexMap.
@@ -26,11 +50,37 @@ class InvertedIndex {
    * @return
    */
   mapWords(wordArray, tag){
+    wordArray.forEach((word) => {
+      if (wordArray[word] in this.indexMap) {
+        this.indexMap[wordArray[word]].push(tag);
+      } else {
+        this.indexMap[wordArray[word]] = [tag];
+      }
+    });
   }
   /**
    * gets the index map
    * @return {object} [all the words that exists in the documents with their tag]
    */
   getIndex() {
+    return this.indexMap;
+  }
+  /**
+   * searches through the index map
+   * @param  {string} query [the set of string being input in search box]
+   * @return {object} result     [the search result is returned]
+   */
+  search(query) {
+    // console.log(query);
+    const dictionary = this.getIndex();
+    const result = {};
+    const searchQuery = query.split(' ');
+    searchQuery.forEach((word) => {
+      if (dictionary.hasOwnProperty(word)){
+        result[word] = this.indexMap[word];
+      }
+    });
+    //console.log(result.keys(word));
+    return Object.keys(result).length > 0 ? result : this.indexMap;
   }
 }
